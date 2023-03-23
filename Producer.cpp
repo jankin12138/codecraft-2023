fb6a6d259dce47ba5bb0e955bfb882556844b031
jankin12138 <101378883+jankin12138@users.noreply.github.com>
2023-03-23 14:51:51 +0800

    修复分配任务逻辑bug

--- Producer.cpp
+++ Producer.cpp
@@ -36,28 +36,28 @@ Task *Producer::get_task() {
         stage_id_ans[7]--;
         count_task_ans[7]++;
     } else if (stage_id_ans[4] != 0 || stage_id_ans[5] != 0 || stage_id_ans[6] != 0) {
-        std::vector<std::vector<int>> tmp_ans = {{4, count_task_ans[4]},
-                                                 {5, count_task_ans[5]},
-                                                 {6, count_task_ans[6]}};
+        std::vector<std::vector<int>> tmp_ans = {{count_task_ans[4],4},
+                                                 {count_task_ans[5],5},
+                                                 {count_task_ans[6],6}};
         sort(tmp_ans.begin(), tmp_ans.end());
-        for (auto a: tmp_ans) {
-            if (stage_id_ans[a[0]] != 0) {
-                tmp = find_stage_id(task_map, a[0]);
-                stage_id_ans[a[0]]--;
-                count_task_ans[a[0]]++;
+        for (auto& a: tmp_ans) {
+            if (stage_id_ans[a[1]] != 0) {
+                tmp = find_stage_id(task_map, a[1]);
+                stage_id_ans[a[1]]--;
+                count_task_ans[a[1]]++;
                 break;
             }
         }
     } else if (stage_id_ans[1] != 0 || stage_id_ans[2] != 0 || stage_id_ans[3] != 0) {
-        std::vector<std::vector<int>> tmp_ans = {{1, count_task_ans[1]},
-                                                 {2, count_task_ans[2]},
-                                                 {3, count_task_ans[3]}};
+        std::vector<std::vector<int>> tmp_ans = {{count_task_ans[1],1},
+                                                 {count_task_ans[2],2},
+                                                 {count_task_ans[3],3}};
         sort(tmp_ans.begin(), tmp_ans.end());
-        for (auto a: tmp_ans) {
-            if (stage_id_ans[a[0]] != 0) {
-                tmp = find_stage_id(task_map, a[0]);//查询其实就是找到一个创建好的任务，等同于在此处创建任务
-                stage_id_ans[a[0]]--;
-                count_task_ans[a[0]]++;
+        for (auto& a: tmp_ans) {
+            if (stage_id_ans[a[1]] != 0) {
+                tmp = find_stage_id(task_map, a[1]);//查询其实就是找到一个创建好的任务，等同于在此处创建任务
+                stage_id_ans[a[1]]--;
+                count_task_ans[a[1]]++;
                 break;
             }
         }
