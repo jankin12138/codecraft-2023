0059e585937ddab7d861e3f61d29809ab9598a6b
Xen39 <2294148317@qq.com>
2023-03-26 15:59:52 +0800

    优化了RobotCrashPreventer内的部分代码

--- RobotCrashPreventer.cpp
+++ RobotCrashPreventer.cpp
@@ -19,16 +19,14 @@ void RobotCrashPreventer::prevent_robot_crash() {
             // 当两机器人可能相撞时，需要根据机器人位置设置一个偏移转向角
             if (will_crash(robots[i], robots[j])) {
                 double rad = calc_rad(robots[i].pos_x - robots[j].pos_x, robots[i].pos_y - robots[j].pos_y);
-//                extra_rotates[i] += (rad - robots[i].pos_rad) * 0.05;
-//                extra_rotates[j] += (rad + pi - robots[j].pos_rad) * 0.05;
-                extra_rotates[i] += mod_pi(rad - robots[i].pos_rad) * 0.3;
-                extra_rotates[j] += mod_pi(rad + pi - robots[j].pos_rad) * 0.3;
+                double ratio = 0.3;
+                extra_rotates[i] += mod_pi(rad - robots[i].pos_rad) * ratio;
+                extra_rotates[j] += mod_pi(rad + pi - robots[j].pos_rad) * ratio;
             }
         }
     }
     for (int i = 0; i < n; ++i) {
         if (extra_rotates[i] != 0) {
-//            cerr << i << " extra rotate " << robots[i].next_rotate + extra_rotates[i] << endl;
             double final_rotate = robots[i].next_rotate + extra_rotates[i];
             robots[i].print_rotate(robots[i].next_rotate + extra_rotates[i]);
         }
@@ -36,7 +34,7 @@ void RobotCrashPreventer::prevent_robot_crash() {
 }
 
 bool RobotCrashPreventer::will_crash(Robot &robot1, Robot &robot2) {
-    if (distance(robot1.pos_x, robot1.pos_y, robot2.pos_x, robot2.pos_y) > 1 + 0.53 * 2)
+    if (distance(robot1.pos_x, robot1.pos_y, robot2.pos_x, robot2.pos_y) > 0.53 * 3)
         return false;
     return true;
 }
